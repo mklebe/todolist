d0f5bfb6baab72566c6088eed4c28fcda4ddcd4b
Matthias Klebe <matthiasklebe@gmail.com>
2021-04-03 18:33:40 +0200

    Enhances UI of feature map

--- components/FeatureAdd.tsx
+++ components/FeatureAdd.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Input, NumberInput, NumberInputField, NumberDecrementStepper, NumberIncrementStepper, NumberInputStepper } from "@chakra-ui/react";
+import { Button, Grid, Input, NumberInput, NumberInputField, NumberDecrementStepper, NumberIncrementStepper, NumberInputStepper, FormControl, FormLabel } from "@chakra-ui/react";
 import { useStore } from "effector-react";
 
 import $featureStore, { addProductFeature, setInitialFeatureEffort, setInitialFeatureImpact, setInitialFeatureTitle } from '../store/feature-store';
@@ -7,33 +7,45 @@ function FeatureAdd() {
   const featureList = useStore($featureStore);
   return (
     <Grid pt={2} templateColumns="8fr 1fr 1fr 2fr" columnGap="3">
-      <Input placeholder='Feature title'
-        value={featureList.initalFeature.title}
-        onChange={(evt) => setInitialFeatureTitle(evt.target.value)} />
-      <NumberInput 
-        max={5}
-        min={1}
-        value={featureList.initalFeature.impact}
-        onChange={(value) => setInitialFeatureImpact( parseInt(value) ) }
-        >
-        <NumberInputField />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
-      <NumberInput
-        max={5}
-        min={1}
-        value={featureList.initalFeature.effort}
-        onChange={(value) => setInitialFeatureEffort(parseInt(value)) }>
-        <NumberInputField />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
-      <Button onClick={() => addProductFeature()}>Add Feature</Button>
+      <FormControl>
+        <FormLabel>Feature Title</FormLabel>
+        <Input placeholder='Add a Feature'
+          value={featureList.initalFeature.title}
+          onChange={(evt) => setInitialFeatureTitle(evt.target.value)} />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Impact</FormLabel>
+        <NumberInput
+          max={5}
+          min={1}
+          value={featureList.initalFeature.impact}
+          onChange={(value) => setInitialFeatureImpact( parseInt(value) ) }
+          >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Effort</FormLabel>
+        <NumberInput
+          max={5}
+          min={1}
+          value={featureList.initalFeature.effort}
+          onChange={(value) => setInitialFeatureEffort(parseInt(value)) }>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+      <FormControl>
+        <FormLabel>&nbsp;</FormLabel>
+        <Button onClick={() => addProductFeature()}>Add Feature</Button>
+      </FormControl>
     </Grid>
   );
 
--- components/FeatureList.tsx
+++ components/FeatureList.tsx
@@ -1,20 +1,26 @@
-import { Input, Button, Flex, Checkbox, Heading, Text, Spacer } from '@chakra-ui/react';
+import { Input, Button, Grid, Checkbox, Heading, Tag, Spacer, Flex, HStack, Container } from '@chakra-ui/react';
 import { useStore } from 'effector-react';
 
-import $store, { deleteProductFeature } from '../store/feature-store';
+import $store, { completeProductFeature, deleteProductFeature } from '../store/feature-store';
 
 function TodoListItems() {
 const store = useStore($store);
 return (
   <>
     {store.features.map(( feature ) => (
-      <Flex pt={2} key={feature.id}>
-        <Text>{feature.title}</Text>
-        <Text>Impact: {feature.impact}</Text>
-        <Text>Effort: {feature.effort}</Text>
-        <Spacer />
-        <Button onClick={() => deleteProductFeature(feature.id)}>Delete Feature</Button>
-      </Flex>
+      <Container maxWidth="100%" mb={4}>
+        <Flex key={feature.id} justifyContent="space-between">
+          <Checkbox isChecked={!!feature.done} onClick={() => completeProductFeature(feature.id)} />
+          <Input mx={2} value={feature.title} readOnly/>
+          <Button onClick={() => deleteProductFeature(feature.id)}>Delete Feature</Button>
+        </Flex>
+        <Flex pt={2}>
+          <HStack spacing={2}>
+            <Tag colorScheme="green">Impact: {feature.impact}</Tag>
+            <Tag colorScheme="yellow">Effort: {feature.effort}</Tag>
+          </HStack>
+        </Flex>
+      </Container>
     ))}
   </>
 )
